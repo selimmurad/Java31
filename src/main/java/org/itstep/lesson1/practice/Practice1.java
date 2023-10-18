61eaebf43215e2cb66a0383e40e68744c8065ab2
DELL <selimmurad1111@gmail>
2023-10-18 15:23:48 +0200

    lesson4

--- src/main/java/org/itstep/lesson1/practice/Practice1.java
+++ src/main/java/org/itstep/lesson1/practice/Practice1.java
@@ -2,17 +2,8 @@
 
 public class Practice1 {
 
+
     public static void main(String[] args) {
 
-          int num1 = 140;
-         int num2 = 26;
+    }}
 
-        System.out.println(num1 + num2);
-        System.out.println(num1 - num2);
-        System.out.println(num1 * num2);
-        System.out.println(num1 / num2);
-        System.out.println((double)num1 / num2);
-
-
-    }
-}
---
+++ src/main/java/org/itstep/lesson4/MathExample.java
@@ -0,0 +1,12 @@
+package org.itstep.lesson4;
+
+public class MathExample {
+  //bibloteka MATH
+
+    public static void main(String[]args){
+        double rowRes = Math.pow(3 ,3);
+        System.out.println("rowRes = " + rowRes);
+
+
+    }
+}
