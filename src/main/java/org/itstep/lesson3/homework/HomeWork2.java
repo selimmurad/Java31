fd40d68ed81a0b3dc241291f42787b1c494f51b8
DELL <selimmurad1111@gmail>
2023-11-13 20:49:43 +0100

    lesson3

--- src/main/java/org/itstep/lesson3/homework/HomeWork2.java
+++ src/main/java/org/itstep/lesson3/homework/HomeWork2.java
@@ -6,5 +6,20 @@ public class HomeWork2 {
 //вывод на консоль, в которой четверти (в прямоугольных (декартовых)
 //системы координат) является точкой. Кварталы обозначены
 //римскими цифрами
+    public static void main(String[] args) {
+        int x = 24;
+        int y = -16;
+        if (x > 0 && y > 0) {
+            System.out.println("I");
+        }
+        if (x < 0 && y > 0) {
+            System.out.println("II");
+        }
+        if (x < 0 && y < 0) {
+            System.out.println("III");
+        }
+        if (x > 0 && y < 0) {
+            System.out.println("IV");
+        }
 
-}
+    }}
