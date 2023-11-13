65e5e452801d839d21c8069e0a8c043f9db22907
DELL <selimmurad1111@gmail>
2023-11-13 21:29:16 +0100

    lesson4

--- src/main/java/org/itstep/lesson4/homework/Task2.java
+++ src/main/java/org/itstep/lesson4/homework/Task2.java
@@ -1,5 +1,7 @@
 package org.itstep.lesson4.homework;
 
+import java.util.Scanner;
+
 public class Task2 {
     //Напишіть програму, яка перевірятиме,
     // чи є введене число з клавіатури,
@@ -7,4 +9,30 @@ public class Task2 {
     // Наприклад, 123454321 або 221122 — паліндром.
     // Програма має вивести YES, якщо число є паліндромом,
     // і NO якщо число не паліндром.
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number ");
+        int palindrom = sc.nextInt();
+        System.out.println(palindrom);
+
+        String palStr = String.valueOf(palindrom);
+
+        boolean b = false;
+
+        for (int i = 0, j = palStr.length() - 1; i < j; i++, j--) {
+            if (palStr.charAt(i) == palStr.charAt(j)) {
+                b = true;
+            } else {
+                b = false;
+                break;
+            }
+        }
+
+
+        if (b == true) {
+            System.out.println("Yes , palindrom");
+        }else {
+            System.out.println("No , palindrom");
+        }
+    }
 }
