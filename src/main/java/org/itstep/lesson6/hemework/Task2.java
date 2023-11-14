4b29acab06fc8843eded9565bdf3d3c2ced055ac
DELL <selimmurad1111@gmail>
2023-11-14 13:51:55 +0100

    lesson6

--- src/main/java/org/itstep/lesson6/hemework/Task2.java
+++ src/main/java/org/itstep/lesson6/hemework/Task2.java
@@ -1,11 +1,59 @@
 package org.itstep.lesson6.hemework;
-
+import java.util.Random;
 public class Task2 {
     //Завдання 8:
-    //В одновимірному масиві, заповненому випадковими числами,
-    //визначте мінімальний та максимальний елементи, підрахуйте
-    //кількість від'ємних та додатних елементів, підрахуйте кількість
-    //нулів. Результати виведіть на екран.
+    //    В одномерном массиве, заполненном случайными числами,
+    //    Определяем минимальное и максимальное количество элементов,
+    //    подсчитываем
+    //    количество отрицательных и положительных элементов,
+    //    подсчитайте количество Нуле
+    //    Отобразите результаты на экране.
+
+        public static void main(String[] args) {
+            int[] array = new int[10];
+            Random random = new Random();
+
+            // Zapolnyat massivi slucaynie znacenie
+
+            for (int i = 0; i < array.length; i++) {
+                array[i] = random.nextInt(30) + 100;
+            }
+
+            // poisk min i max element
+
+            int min = array[0];
+            int max = array[0];
+            for (int value : array) {
+                if (value < min) {
+                    min = value;
+                }
+                if (value > max) {
+                    max = value;
+                }
+            }
+
+            // padscot otricatelnie , polojetelnie i nuley.
+            int negativeCount = 0;
+            int positiveCount = 0;
+            int zeroCount = 0;
+            for (int value : array) {
+                if (value < 0) {
+                    negativeCount++;
+                } else if (value > 0) {
+                    positiveCount++;
+
+                    zeroCount++;
+                }
+            }
+
+            System.out.println("Масив: " + java.util.Arrays.toString(array));
+            System.out.println("Міn element: " + min);
+            System.out.println("Маx element: " + max);
+            System.out.println("otricatelnie element: " + negativeCount);
+            System.out.println("polojitelnie element: " + positiveCount);
+            System.out.println("kolicestvo nul: " + zeroCount);
+        }
+    }
 
 
-}
+
--- src/main/java/org/itstep/lesson6/hemework/Task3.java
+++ src/main/java/org/itstep/lesson6/hemework/Task3.java
@@ -1,11 +1,41 @@
 package org.itstep.lesson6.hemework;
+import java.util.Random;
 
 public class Task3 {
     //Завдання 10
-    //Заповніть масив розмірності n випадковими цифрами від 0 до
-    //33. Знайдіть елемент масиву, який ділиться без остачі на
-    //значення елемента зліва і значення елемента справа. Виведіть в
-    //консоль значення початкового масиву. Виведіть індекс
-    //знайденого елемента. Якщо такий не знайдено,виведіть -1. n —
-    //задається з клавіатури.
-}
+    //Заполните массив размерности n случайными цифрами от 0 до 33.
+    //    Найти элемент массива, который делится на
+    //    Значение элемента слева и значение элемента справа.
+    //    Вывод в консоль значения исходного массива.
+    //    Распечатать индекс найденного элемента .
+    //    Если ничего не найдено, выведите -1 .
+
+
+        public static void main(String[] args) {
+
+            int n = 10;  // Razmer masiva mojno zamenit
+            int[] array = new int[n];
+            Random random = new Random();
+
+            // Zopolnyat massiv ot 0 до 33
+
+            for (int i = 0; i < array.length; i++) {
+                array[i] = random.nextInt(34);  // Qeneracie  ot 0 до 33
+            }
+
+            // Poysk potxadyasie elementov
+
+            int foundIndex = -1;  //index naidennogo element, po umolchaniyu -1 (esli nichego nay naydeno)
+            for (int i = 1; i < array.length - 1; i++) {
+                if (array[i] % array[i - 1] == 0 && array[i] % array[i + 1] == 0) {
+                    foundIndex = i;
+
+                }
+            }
+            System.out.println("Massiv: " + java.util.Arrays.toString(array));
+          //  System.out.println("index naydennoqo element: " + foundIndex);
+
+
+        }
+    }
+
