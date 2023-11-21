3cd4ba9ce10dd244b1e91a1388fab0ccd3902328
DELL <selimmurad1111@gmail>
2023-11-21 19:15:21 +0100

    lesson8

--- src/main/java/org/itstep/lesson8/homework/Task1.java
+++ src/main/java/org/itstep/lesson8/homework/Task1.java
@@ -1,5 +1,5 @@
 package org.itstep.lesson8.homework;
-
+import java.util.Scanner;
 public class Task1 {
     //Завдання 2
     //Напишіть програму, яка створить рядок, в якому знаходяться всі
@@ -8,4 +8,31 @@ public class Task1 {
     //більше 1 000 символів.
     //За числом n (введеного з клавіатури), виведіть цифру на
     //n-й позиції (використовується нумерація з 1).
-}
+   //
+
+
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.print("Введіть позицію (n): ");
+            int n = scanner.nextInt();
+
+            StringBuilder sequence = new StringBuilder();
+
+            // Додаємо цілі числа у послідовність до тих пір, поки рядок не перевищить 1000 символів
+            for (int i = 1; sequence.length() < 1000; i++) {
+                sequence.append(i);
+            }
+
+            // Виводимо всю послідовність чисел
+            System.out.println("Послідовність цілих чисел: " + sequence);
+
+            // Отримуємо цифру на вказаній позиції
+            char digit = sequence.charAt(n - 1);
+            System.out.println("Цифра на позиції " + n + ": " + digit);
+
+            scanner.close();
+        }
+    }
+
+
--- src/main/java/org/itstep/lesson8/homework/Task2.java
+++ src/main/java/org/itstep/lesson8/homework/Task2.java
@@ -1,9 +1,33 @@
 package org.itstep.lesson8.homework;
-
+import java.util.Scanner;
 public class Task2 {
     //Завдання 4:
-    //Користувач вводить з клавіатури рядок. Перевірте, чи є введений
+    //kористувач вводить з клавіатури рядок. Перевірте, чи є введений
     //рядок паліндромом. Паліндроми — слова, речення або текст, які
     //однаково читаються як зліва направо, так і справа наліво.
     //Наприклад: кок; козак з казок; радар; а мене нема.
-}
+
+
+
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.print("Введіть рядок: ");
+            String inputString = scanner.nextLine();
+
+            String originalString = inputString.replaceAll("\\s", "").toLowerCase();
+
+            // Перевертаємо рядок
+            String reversedString = new StringBuilder(originalString).reverse().toString();
+
+            // Перевіряємо, чи є рядок паліндромом
+            if (originalString.equals(reversedString)) {
+                System.out.println("Введений рядок є паліндромом");
+            } else {
+                System.out.println("Введений рядок не є паліндромом");
+            }
+
+            scanner.close();
+        }
+    }
+
